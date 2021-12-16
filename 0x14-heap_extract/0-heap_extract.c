31a6d73e798c084157d60cd92d297e052afffaa0
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2021-12-16 09:17:49 -0500

    adding project

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+* heap_extract - extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of the heap
+* Return: value stored in the root node.
+* If your function fails, return 0
+*/
+int heap_extract(heap_t **root)
+{
+	if (!root)
+		return (0);
+
+	return (0);
+}
